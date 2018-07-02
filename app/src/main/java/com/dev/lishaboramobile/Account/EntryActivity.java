c8baf79d9502568fb27d9f065c49c94806b9b4a3
erickogi <erickogi14@gmail.com>
2018-07-02 09:24:29 +0300

    New

---
+++ app/src/main/java/com/dev/lishaboramobile/Account/EntryActivity.java
@@ -0,0 +1,15 @@
+package com.dev.lishaboramobile.Account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dev.lishaboramobile.R;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+}
